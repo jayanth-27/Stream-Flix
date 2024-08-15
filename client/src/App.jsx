55078034a4e38a26aef7fa79774df2b277f29700
jayanth-27 <jayanth.reddy178@gmail.com>
2024-08-15 17:28:05 -0400

    adding react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,26 @@ import Home from "./home_page/Home"
 import Watch from "./pages/watch/Watch";
 import Register from "./pages/register/Register";
 import Login from "./pages/login/Login"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App()
 {
-  return <Home/>;
+  return (
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/movies">
+        < Home />
+      </Route>
+    </Switch>
+  </Router>
+  );
 }
 
 export default App;
